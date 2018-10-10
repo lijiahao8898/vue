743cbfe84383997181a2360a791e67a96442092a
Bloodborne <472239548@qq.com>
2018-10-10 15:43:22 +0800

    Update index.js

--- src/core/index.js
+++ src/core/index.js
@@ -1,8 +1,12 @@
+// 这个应该是实例化的引入
 import Vue from './instance/index'
+// 这个应该是初始化一些全局API
 import { initGlobalAPI } from './global-api/index'
+// 这个应该是从判断执行环境中的引入是否是ssr环境，是一个Boolea类型
 import { isServerRendering } from 'core/util/env'
+// 这个应该是virtualDom编译成renderContext的方法
 import { FunctionalRenderContext } from 'core/vdom/create-functional-component'
-
+// 这里开始执行初始化全局变量
 initGlobalAPI(Vue)
 
 Object.defineProperty(Vue.prototype, '$isServer', {
